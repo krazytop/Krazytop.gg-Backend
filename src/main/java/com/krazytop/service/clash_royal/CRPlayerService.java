d8bec091b5449d187d56ef4417c96ccae1605f4e
clement_tell <clement.tell44@gmail.com>
2024-06-18 19:57:12 +0200

    fix CR

--- src/main/java/com/krazytop/service/clash_royal/CRPlayerService.java
+++ src/main/java/com/krazytop/service/clash_royal/CRPlayerService.java
@@ -59,6 +59,7 @@ public class CRPlayerService {
 
     private void accountLevelEnrichment(CRPlayerEntity player) {
         CRAccountLevelEntity accountLevel = new ModelMapper().map(accountLevelNomenclatureRepository.findFirstByLevel(player.getExpLevel()), CRAccountLevelEntity.class);
+        System.out.println("Account level : " + accountLevel);
         accountLevel.setCurrentExp(player.getExpPoints());
         player.setAccountLevel(accountLevel);
     }
