f5a4cc649422952faac75b053860a5090d322d29
Krazytop <clement.tell44@gmail.com>
2024-12-08 10:13:05 +0100

    sonar

--- src/main/java/com/krazytop/repository/tft/TFTUnitNomenclatureRepository.java
+++ src/main/java/com/krazytop/repository/tft/TFTUnitNomenclatureRepository.java
@@ -2,7 +2,6 @@ package com.krazytop.repository.tft;
 
 import com.krazytop.nomenclature.tft.TFTUnitNomenclature;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 public interface TFTUnitNomenclatureRepository extends MongoRepository<TFTUnitNomenclature, String> {
 
